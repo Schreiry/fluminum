3f3c6d75d714522e99701ec53f8e6f5132fdb81b
Schreiry <108213850+Schreiry@users.noreply.github.com>
2025-08-08 14:22:35 +0400

    Delete

--- Interactive.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Common.h"
-
-// The main loop for a single operation in interactive mode
-void run_one_operation();
-
-// Displays the main interactive menu to start the program loop
-void run_interactive_mode();
